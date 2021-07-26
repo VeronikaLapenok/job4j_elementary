df584a9463d03f534e9b5d4f0812e74c5c7087ca
Veronika Lapenok <veronika.lapenok@mail.ru>
2021-07-26 12:18:28 +0300

    4.3.6. Math.abs [#373616]

--- src/ru/job4j/condition/ChessBoard.java
+++ src/ru/job4j/condition/ChessBoard.java
@@ -1,5 +1,15 @@
 package ru.job4j.condition;
 
 public class ChessBoard {
+    public static int way(int x1, int y1, int x2, int y2) {
+        int result = 0;
+        if (Math.abs(x2 - x1) == Math.abs(y2 - y1)) {
+            result = Math.abs(x2 - x1);
+        }
+        return result;
+    }
 
+    public static void main(String[] args) {
+        System.out.println(way(2, 6, 4, 1));
+    }
 }
