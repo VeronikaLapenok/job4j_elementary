0c74c4e4d3da20ce4a454df80a5e374205b9e057
Veronika Lapenok <veronika.lapenok@mail.ru>
2021-08-11 16:29:51 +0300

    6.7.4. Выигрышные комбинации в сокобан [#53859]

--- src/ru/job4j/array/MatrixCheck.java
+++ src/ru/job4j/array/MatrixCheck.java
@@ -1,6 +1,17 @@
 package ru.job4j.array;
 
 public class MatrixCheck {
+    public static boolean isWin(char[][] board) {
+        boolean result = false;
+        for (int index = 0; index < board.length; index++) {
+            if (board[index][index] == 'X' && (monoHorizontal(board, index) || monoVertical(board, index))) {
+                result = true;
+                break;
+            }
+        }
+        return result;
+    }
+
     public static char[] extractDiagonal(char[][] board) {
         char[] rsl = new char[board.length];
         for (int index = 0; index < board.length; index++) {
